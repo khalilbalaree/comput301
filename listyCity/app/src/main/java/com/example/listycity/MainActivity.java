3f5cde7564c3c0a43180370f3935dae959a94253
zijun wu <zijuncanada@gmail.com>
2022-03-09 13:02:33 -0700

    updata

---
+++ listyCity/app/src/main/java/com/example/listycity/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.listycity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ListView cityList;
+    ArrayAdapter<String> cityAdapter;
+    ArrayList<String> dataList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cityList = findViewById(R.id.city_list);
+
+        String []cities = {"Edmonton", "Vancouver"};
+
+        dataList = new ArrayList<>();
+        dataList.addAll(Arrays.asList(cities));
+
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+
+        cityList.setAdapter(cityAdapter);
+
+        dataList.add("new is my new city");
+
+        cityAdapter.notifyDataSetChanged();
+
+
+    }
+}
